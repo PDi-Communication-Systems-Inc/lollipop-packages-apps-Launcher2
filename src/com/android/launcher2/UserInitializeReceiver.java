ee270366570220ce7f79f3c0067238dd1f364666
mrobbeloth-pdi <mrobbeloth@pdiarm.com>
2016-07-13 10:21:37 -0400

    Force use of default wallpaper on restricted patient profiles as set by facility administrator

--- src/com/android/launcher2/UserInitializeReceiver.java
+++ src/com/android/launcher2/UserInitializeReceiver.java
@@ -20,7 +20,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import com.android.launcher.R;
-
+import android.util.Log;
 import android.app.WallpaperManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -32,39 +32,10 @@ import android.content.res.Resources;
  * first wallpaper that is not in use by another user.
  */
 public class UserInitializeReceiver extends BroadcastReceiver {
+	static final String TAG = "Launcher_UserInitializeReceiver";
+	
     @Override
     public void onReceive(Context context, Intent intent) {
-        final Resources resources = context.getResources();
-        // Context.getPackageName() may return the "original" package name,
-        // com.android.launcher2; Resources needs the real package name,
-        // com.android.launcher. So we ask Resources for what it thinks the
-        // package name should be.
-        final String packageName = resources.getResourcePackageName(R.array.wallpapers);
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        addWallpapers(resources, packageName, R.array.wallpapers, list);
-        addWallpapers(resources, packageName, R.array.extra_wallpapers, list);
-        WallpaperManager wpm = (WallpaperManager) context.getSystemService(
-                Context.WALLPAPER_SERVICE);
-        for (int i=1; i<list.size(); i++) {
-            int resid = list.get(i);
-            if (!wpm.hasResourceWallpaper(resid)) {
-                try {
-                    wpm.setResource(resid);
-                } catch (IOException e) {
-                }
-                return;
-            }
-        }
-    }
-
-    private void addWallpapers(Resources resources, String packageName, int resid,
-            ArrayList<Integer> outList) {
-        final String[] extras = resources.getStringArray(resid);
-        for (String extra : extras) {
-            int res = resources.getIdentifier(extra, "drawable", packageName);
-            if (res != 0) {
-                outList.add(res);
-            }
-        }
+    	// Force use of default wallpaper used by administrator
     }
 }
